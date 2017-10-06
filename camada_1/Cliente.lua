bd72626d2fb26d20ae68c409ff906141ca54c77e
Andre Borges <albo172@hotmail.com>
2017-10-06 07:36:07 -0300

    Modificado servidor

--- camada_1/Cliente.lua
+++ camada_1/Cliente.lua
@@ -1,37 +1,60 @@
+local MAX_TAM = 1000;
+
+function toBits(num)
+  alg = tonumber(num)
+	print (alg)
+	-- returns a table of bits, least significant first.
+    local t={} -- will contain the bits
+    while alg > 0 do
+        rest=math.fmod(alg ,2)
+		print(rest)
+        t[#t+1]=rest
+        alg=(alg -rest)/2
+    end
+    return t
+end
+
+function numberstring(number)
+   local s = ""
+   repeat
+      local remainder = math.fmod(number,2)
+      s = remainder..s
+      number = (number-remainder)/2
+   until number==0
+   return s
+end
+
 require('socket')
 
 io.write "Escolha um servidor > " ;
 
-local servidor = io.read();							--serve pra me irritar e
-												--eu ter que digitar localhost toda vez que eu for testar
+local servidor = io.read();
 
 io.write("Escolha uma porta > ");
 
-local porta = io.read();								--grava em porta a porta em que se deseja conectar
+local porta = io.read();
 
-local Vcliente = socket.connect(servidor, porta);		--tenta conectar Vcliente a um host "remoto",
-												--retornando e guardando em Vcliente um objeto cliente
-												--(capaz de executar certas funcoes)
+local Vcliente = socket.connect(servidor, porta);
 
-if Vcliente then								--caso Vcliente nao seja nulo, faca o seguinte
+if Vcliente then
 	io.write("Conectado com sucesso!\n");
 end
 
 io.write("Digite o nome do arquivo: ")
 local filename = io.read()
 
-local file = assert(io.open(filename, 'r')) 		--abre b.txt em modo de leitura
+local file = assert(io.open(filename, 'r'))
 
-local data = file:read('*all')						--Le conteudo de b.txt
+local data = file:read('*all') -- Message
 
 -- Simulating package lose
 math.randomseed(os.time())
 packet_lose_chance = math.random() % 10
 
-if(packet_lose_chance < 5) then
-  print("PACOTE PERDIDO \"NO CAMINHO\".")
-else
-  Vcliente:send(data)
+while(packet_lose_chance < 1) then -- 10% de chance de perda de pacote
+  print("PACOTE PERDIDO \"NO \"CAMINHO ")
+  print("TENTANDO NOVAMENTE")
 end
+Vcliente:send(data)
 
 Vcliente:close()
--- camada_1/Servidor.lua
+++ camada_1/Servidor.lua
@@ -14,6 +14,27 @@ local conexao = assert(server:accept())			--espera um cliente se conectar ao obj
 
 reply = conexao:receive('*a');				--tenta receber dados de um objeto cliente chamado Vcliente
 
+local preambule = reply[0-55]
+local frame_delimiter = reply[56-63]
+local mac_destination = reply[64-111]
+local mac_source = reply[112-159]
+local tag = reply[160-191]
+local ethertype = reply[192-207]
+local payload = reply[208-567]
+local frame_check_sequence = [568-599]
+local interpacket_gap = [600-695]
+
+print("Preambulo: " .. preambule)
+print("Delimitador: " .. frame_delimiter)
+print("Mac de destino: " .. mac_destination)
+print("Mac da fonte: " .. mac_source)
+print("Tag: " .. tag)
+print("Ethertype: " .. ethertype)
+print("Payload: " .. payload)
+print("Frame check sequence: " .. frame_check_sequence)
+print("Interpacket gap: " .. interpacket_gap)
+
+
 print("Mensagem : " .. reply)
 
 conexao:close()
