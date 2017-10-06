c6d62c2a6d5079f1252985227e0c721b06d76118
BrunoDamacena <damacena.bruno@gmail.com>
2017-10-06 08:36:21 -0300

    Alterado codigo do servidor

--- camada_1/Servidor.lua
+++ camada_1/Servidor.lua
@@ -6,7 +6,7 @@ local server = assert(socket.bind("*", 0)) 		--"amarra" a variavel socket ao loc
 local ip, port = server:getsockname()			--retorna a informacao de endereco associada ao objeto
 												--server(IP e porta)
 
-print("Conecte ao endereco localhost na porta " .. port)--nos fala a real
+print("Conecte ao endereco na porta " .. port)--nos fala a real
 
 local conexao = assert(server:accept())			--espera um cliente se conectar ao objeto server e
 												--retorna um objeto cliente ao qual server esta
@@ -14,27 +14,27 @@ local conexao = assert(server:accept())			--espera um cliente se conectar ao obj
 
 reply = conexao:receive('*a');				--tenta receber dados de um objeto cliente chamado Vcliente
 
-local preambule = reply[0-55]
-local frame_delimiter = reply[56-63]
-local mac_destination = reply[64-111]
-local mac_source = reply[112-159]
-local tag = reply[160-191]
-local ethertype = reply[192-207]
-local payload = reply[208-567]
-local frame_check_sequence = [568-599]
-local interpacket_gap = [600-695]
+-- local preambule = reply[0-55]
+-- local frame_delimiter = reply[56-63]
+-- local mac_destination = reply[64-111]
+-- local mac_source = reply[112-159]
+-- local tag = reply[160-191]
+-- local ethertype = reply[192-207]
+-- local payload = reply[208-567]
+-- local frame_check_sequence = [568-599]
+-- local interpacket_gap = [600-695]
+--
+-- print("Preambulo: " .. preambule)
+-- print("Delimitador: " .. frame_delimiter)
+-- print("Mac de destino: " .. mac_destination)
+-- print("Mac da fonte: " .. mac_source)
+-- print("Tag: " .. tag)
+-- print("Ethertype: " .. ethertype)
+-- print("Payload: " .. payload)
+-- print("Frame check sequence: " .. frame_check_sequence)
+-- print("Interpacket gap: " .. interpacket_gap)
 
-print("Preambulo: " .. preambule)
-print("Delimitador: " .. frame_delimiter)
-print("Mac de destino: " .. mac_destination)
-print("Mac da fonte: " .. mac_source)
-print("Tag: " .. tag)
-print("Ethertype: " .. ethertype)
-print("Payload: " .. payload)
-print("Frame check sequence: " .. frame_check_sequence)
-print("Interpacket gap: " .. interpacket_gap)
-
-
+print(string.strbyte(reply[0-7]))
 print("Mensagem : " .. reply)
 
 conexao:close()
